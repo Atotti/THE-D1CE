fdbf41f78b6a966c4cc4788f3b707b87d5f4fcaf
Ayuto Tsutsumi <56724676+Atotti@users.noreply.github.com>
2024-10-22 01:40:56 +0900

    fix url encode

--- Assets/GenerateQr.cs
+++ Assets/GenerateQr.cs
@@ -12,6 +12,7 @@ public class GenerateQr : MonoBehaviour
 
     void Start()
     {
+        qrText = "https://twitter.com/intent/tweet?text=AR%E4%BC%9A%E3%81%AE%E3%82%B2%E3%83%BC%E3%83%A0THE%20D1CE%E3%81%A7" + ScoreManager.instance.score.ToString() + "%E7%82%B9%E3%82%92%E7%8D%B2%E5%BE%97%E3%81%97%E3%81%BE%E3%81%97%E3%81%9F&url=https://xi-server.ayutaso.com/ranking/&hashtags=AR会&via=arcircle";
         // QRコード生成処理
         EncodedQRTextire = new Texture2D(QrTxtureW, QrTxtureH);
         var color32 = Encode(qrText, EncodedQRTextire.width, EncodedQRTextire.height);
